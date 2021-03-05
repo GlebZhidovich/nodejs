6d8d1694061e35178400f308246158a63a4ce040
GlebVetrov <5925520@gmail.com>
2021-03-05 18:09:50 +0300

    feat: add logging and errors listeners

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('hello22')
+const http = require("http");
+const { nanoid } = require('nanoid');
+
+process.on('uncaughtException', err => {
+    console.error(err.message);
+});
+
+process.on('unhandledRejection', err => {
+    console.error(err.message);
+});
+
+http.createServer(listenServer).listen(3000);
+
+function logging(request) {
+    console.log("Url: " + request.url);
+    console.log("Тип запроса: " + request.method);
+}
+
+function listenServer(request, response){
+    logging(request);
+    const id = nanoid();
+    response.end("Hello world! " + id);
+}
