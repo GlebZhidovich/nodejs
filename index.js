a93cbe92e6a0dfe53e2564724c8098bca58d6294
GlebZhidovich <gzhidovich@gmail.com>
2021-03-17 21:22:17 +0300

    refactor: add func for router access

--- index.js
+++ index.js
@@ -1,37 +1,51 @@
-const http = require("http");
-const { nanoid } = require('nanoid');
+const http = require('http');
+const router = require('./src/projects.router')
+const { getBody } = require('./src/helpers');
 
 process.on('uncaughtException', err => {
-    console.error(err.message);
+  console.error('Uncaught Exception:', err.message);
 });
 
 process.on('unhandledRejection', err => {
-    console.error(err.message);
+  console.error('Unhandled Rejection:', err.message);
 });
 
-http.createServer(listenServer).listen(3000);
+http.createServer(listenServer).listen(3000, '127.0.0.1', () => {
+  console.log('Сервер начал прослушивание запросов');
+});
 
-function logging(request) {
-    console.log("Url: " + request.url);
-    console.log("Тип запроса: " + request.method);
+function logging({ url, method, body }) {
+  console.log('Url: ' + url);
+  console.log('Тип запроса: ' + method);
+  console.log('Body: ' + body);
 }
 
-async function getBody(request) {
-    return new Promise((resolve) => {
-        let body = [];
-        request
-            .on('data', (chunk) => {
-                body.push(chunk);
-            })
-            .on('end', () => {
-                resolve(Buffer.concat(body).toString());
-            });
-    });
+function isProductsUrl(url) {
+  const urlName = 'projects';
+  const arrUrl = url.split('/');
+  const maxParams = 4;
+  return arrUrl.includes(urlName) && arrUrl.length < maxParams;
 }
 
-async function listenServer(request, response) {
-    logging(request);
-    const body = await getBody(request);
-    console.log(JSON.parse(body))
-    response.end("Hello world! ");
+function isInRouter(method) {
+  const methodName = `${method.toLowerCase()}Method`;
+  return router[methodName];
+}
+
+function isAccess(url, method) {
+  return isProductsUrl(url) && isInRouter(method);
+}
+
+async function listenServer(req, res) {
+  const { url, method } = req;
+  const body = await getBody(req);
+  logging({ url, method, body });
+    console.log(isProductsUrl(url))
+  if (isAccess(url, method)) {
+    router[`${method.toLowerCase()}Method`](req, res);
+    return;
+  }
+
+  res.statusCode = 404;
+  res.end('Not found!');
 }
