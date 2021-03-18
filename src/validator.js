39116844ea02f4b0532bf24ff0eb142479c5a0ad
GlebZhidovich <gzhidovich@gmail.com>
2021-03-18 12:47:02 +0300

    feat: add two validate func

--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,15 @@
-const isProject = (project) => {
+const isStringValues = (project) => {
   return Object.values(project).every(val => typeof val === 'string');
+}
+
+const isOnlyCurrentValues = (project) => {
+  const values = ['name', 'description', 'startDate', 'endDate'];
+  const keys = Object.keys(project);
+  return keys.length === values.length && keys.every(key => values.includes(key));
+}
+
+const isProject = (project) => {
+  return isStringValues(project) && isOnlyCurrentValues(project);
 }
 
 module.exports = {
